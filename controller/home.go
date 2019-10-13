401a045b0675af345eafff7380302b4717ea40cc
Bruno Ramalho <bfmramalho@gmail.com>
2019-10-13 19:02:53 +0200

    home controller

---
+++ controller/home.go
@@ -0,0 +1,12 @@
+package controller
+
+import (
+	"html/template"
+	"net/http"
+)
+
+// HomeHandler controller method
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("web/template/index.html"))
+	tmpl.Execute(w, nil)
+}
