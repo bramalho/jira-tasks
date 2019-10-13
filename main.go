1be4aa3a3cb55fd92b22611c5e3eccfa155c30bd
Bruno Ramalho <bfmramalho@gmail.com>
2019-10-13 19:03:10 +0200

    main

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/bramalho/jira-tasks/controller"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", controller.HomeHandler).Methods("GET")
+
+	router.HandleFunc("/ok", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	})
+
+	log.Println("Application is running on localhost:8080")
+
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         "localhost:8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
